a79d3003f03680c656b4b323abfe988bbb3cc774
ivz-dev <zakutynsky@yahoo.com>
2017-04-24 10:34:06 +0300

    fixed MemberConversations bug

--- src/Models/facebookAuth.php
+++ src/Models/facebookAuth.php
@@ -6,15 +6,41 @@ class facebookAuth
     public static function getImpersonateToken($memberId, $apiUrl ,$client, $accessToken)
     {
         $query_str = $apiUrl . "/$memberId";
-        $resp = $client->get($query_str, [
-            'headers' => [
-                'Authorization' => "Bearer ".$accessToken,
-            ],
-            'query' => array('fields'=>'impersonate_token')
-        ]);
 
-        $responseBody = $resp->getBody()->getContents();
+        try {
+            $resp = $client->get($query_str, [
+                'headers' => [
+                    'Authorization' => "Bearer ".$accessToken,
+                ],
+                'query' => array('fields'=>'impersonate_token')
+            ]);
 
-        return json_decode($responseBody)->impersonate_token;
+            $responseBody = $resp->getBody()->getContents();
+
+
+
+            if(in_array($resp->getStatusCode(), ['200', '201', '202', '203', '204'])) {
+                return json_decode($responseBody)->impersonate_token;
+            } else {
+                $result['callback'] = 'error';
+                $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+                $result['contextWrites']['to']['status_msg'] = json_decode($responseBody);
+                die();
+            }
+        } catch (\GuzzleHttp\Exception\ClientException $exception) {
+
+            $responseBody = $exception->getResponse()->getBody()->getContents();
+            if(empty(json_decode($responseBody))) {
+                $out = $responseBody;
+            } else {
+                $out = json_decode($responseBody);
+            }
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = $out;
+
+        }
+
+
     }
 }
